7fdb5ce8d9df0913496200eae87cb0df295418ea
SergeyGolenko <1989sergiotay@gmail.com>
2020-12-28 00:11:01 +0200

    get info with coreData

--- Core Data Demo/ViewController.swift
+++ Core Data Demo/ViewController.swift
@@ -6,12 +6,56 @@
 //
 
 import UIKit
+import CoreData
+
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        let appDelegate = UIApplication.shared.delegate as? AppDelegate
+        let context = appDelegate?.persistentContainer.viewContext
+         //create new user
+        let newUser = NSEntityDescription.insertNewObject(forEntityName: "Users", into: context!)
+        newUser.setValue("rob", forKey: "username")
+        newUser.setValue("myparol", forKey: "password")
+        newUser.setValue(31, forKey: "age")
+        
+        do {
+            try context?.save()
+            print("Save there")
+            
+        } catch {
+            print("There was an error")
+        }
+        
+        
+        
+        //grab saved info with coreData
+        let request = NSFetchRequest<NSFetchRequestResult>(entityName: "Users")
+        request.returnsObjectsAsFaults = false
+        do {
+            let results = try context?.fetch(request)
+            
+            if results!.count > 0 {
+                
+                for i in results as! [NSManagedObject]{
+                    if let userName = i.value(forKey: "username") as? String{
+                        print(userName)
+                    }
+                    if let parol = i.value(forKey: "password") as? String {
+                        print(parol)
+                    }
+                    if let age = i.value(forKey: "age") as? Int{
+                        print(age)
+                    }
+                }
+            }
+        } catch {
+            print("Could not fetch results")
+        }
+        
     }
 
 
